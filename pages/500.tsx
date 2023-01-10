29fe182c6ac01089fa7ee37927ba56712a8cf26d
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-01-09 18:00:24 -0800

    error

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,25 +1,13 @@
 import type { NextPage } from "next";
 
-const Error: NextPage<{ statusCode?: number }> = ({ statusCode }) => {
-  console.log({ statusCode });
+const Error = (props:any) => {
+  console.log(JSON.stringify(props));
   return (
     <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
+      {JSON.stringify(props)}
     </p>
   );
 };
 
-// various log checks
-Error.getInitialProps = ({ res, err }) => {
-  console.log("err", err);
-  console.log("res", res);
-  const clientSideError = res ? res.statusCode : Boolean(err);
-  console.log({ clientSideError });
-  console.log("server", Boolean(res));
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
 
 export default Error;
