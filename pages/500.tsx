18310938f6fcf0643ec951372ba306e0152d8c6e
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-01-09 17:54:06 -0800

    updated 500

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,11 +1,25 @@
-function Error({ statusCode }:any) {
-    return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
-  
-  export default Error
+import type { NextPage } from "next";
+
+const Error: NextPage<{ statusCode?: number }> = ({ statusCode }) => {
+  console.log({ statusCode });
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : "An error occurred on client"}
+    </p>
+  );
+};
+
+// various log checks
+Error.getInitialProps = ({ res, err }) => {
+  console.log("err", err);
+  console.log("res", res);
+  const clientSideError = res ? res.statusCode : Boolean(err);
+  console.log({ clientSideError });
+  console.log("server", Boolean(res));
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
