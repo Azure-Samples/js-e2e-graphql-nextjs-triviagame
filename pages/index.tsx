bc08734326eb7d916ef14cd0b38a84843bf8b146
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-01-10 14:23:52 -0800

    error on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Error from 'next/error'
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { getExistingContainer } from "../azure/azureCosmosdb";
@@ -23,6 +24,8 @@ export async function getServerSideProps() {
     props: {
       buildOn: new Date().toLocaleString(),
       count: items[0].count, // Should equal CONSTANTS.MAX_ITEMS_IN_DATABASE (100)
+      error: null,
+      success: true
     },
   };
   } catch (error) {
@@ -30,54 +33,65 @@ export async function getServerSideProps() {
       props: {
         buildOn: new Date().toLocaleString(),
         count: 0,
+        error,
+        success: false
       },
     };
   } 
 
 }
 
-const Home: NextPage<{ count: number; buildOn: string }> = (props) => {
+const Home: NextPage<{ count: number; buildOn: string, error: Error, success: boolean }> = (props) => {
   return (
     <>
-      <Navbar />
-      <div className={styles.container}>
-        <Head>
-          <title>Test your trivia skills</title>
-          <meta name="description" content="An awesome trivia app" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+      if(error){
+        <>
+          {JSON.stringify(props.error)}
+          <Error statusCode={JSON.stringify(props.error) as never} />
+        </>
+      } else {
+        <>
+        <Navbar />
+          <div className={styles.container}>
+            <Head>
+              <title>Test your trivia skills</title>
+              <meta name="description" content="An awesome trivia app" />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>Trivia time!</h1>
+            <main className={styles.main}>
+              <h1 className={styles.title}>Trivia time!</h1>
 
-          <Question count={props.count} gameQuestionCount={5} />
-        </main>
+              <Question count={props.count} gameQuestionCount={5} />
+            </main>
 
-        <footer className={styles.footer}>
-          Built on: {props.buildOn}
-          <a
-            href="https://nextjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Build with Next.js
-          </a>
-          <a
-            href="https://docs.microsoft.com/azure/static-web-apps/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by Azure
-          </a>
-          <a
-            href="https://github.com/aaronpowell/nextjs-graphql-trivia-demo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open Sourced on GitHub
-          </a>
-        </footer>
-      </div>
+            <footer className={styles.footer}>
+              Built on: {props.buildOn}
+              <a
+                href="https://nextjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Build with Next.js
+              </a>
+              <a
+                href="https://docs.microsoft.com/azure/static-web-apps/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Powered by Azure
+              </a>
+              <a
+                href="https://github.com/aaronpowell/nextjs-graphql-trivia-demo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open Sourced on GitHub
+              </a>
+            </footer>
+          </div>
+        </>
+      }
     </>
   );
 };
